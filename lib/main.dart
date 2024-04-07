c028d464e477d3e8f45e277488de9e761ad3c95d
PrinceRathee01 <pranaya107@gmail.com>
2024-04-07 18:38:21 +0530

    Final Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,24 @@ class XylophoneApp extends StatelessWidget {
     player.play(AssetSource('note$soundnumber.wav'));
   }
   
+Expanded buildkey({int soundnumber=1, Color? color}){
+  return Expanded(
+                  child: TextButton(
+                    
+                    style: TextButton.styleFrom(
+                      backgroundColor: color,
+                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero,
+                      side: BorderSide(color: color!))
+                    ),
+                    
+                    onPressed: (){
+                  
+                    playsound(soundnumber);
+                    
+                  }, child: Text('')),
+                );
+}
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -23,111 +41,14 @@ class XylophoneApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
                  
-                Expanded(
-                  child: TextButton(
-                    
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.red,
-                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero,
-                      side: BorderSide(color: Colors.red))
-                    ),
-                    
-                    onPressed: (){
-                  
-                    playsound(1);
-                    
-                  }, child: Text('')),
-                ),
-
-                Expanded(
-                  child: TextButton(
-                    
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-                      side: BorderSide(color: Colors.orange)
-                    ),
-                    
-                    onPressed: (){
-                    playsound(2);
-                    
-                  }, child: Text('')),
-                ),
-
-                Expanded(
-                  child: TextButton(
-                    
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.yellow,
-                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-                      side: BorderSide(color: Colors.yellow)
-                    ),
-                  
-                    onPressed: (){
-                    playsound(3);
-                    
-                  }, child: Text('')),
-                ),
-
-                Expanded(
-                  child: TextButton(
-                  
-                    style: TextButton.styleFrom(
-                    backgroundColor: Colors.green,
-                    shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-                    side: BorderSide(color: Colors.green)
-                    ),
-                           
-                    onPressed: (){
-                    playsound(4);
-                    
-                  }, child: Text('')),
-                ),
-
-                Expanded(
-                  child: TextButton(
-                    
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.teal,
-                      shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-                    side: BorderSide(color: Colors.teal)
-                    ),
-                    
-                    onPressed: (){
-                    playsound(5);
-                    
-                  }, child: Text('')),
-                ),
-
-              Expanded(
-                child: TextButton(
-                  
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-                    side: BorderSide(color: Colors.blue)
-                  ),
-                  
-                  onPressed: (){
-                 playsound(6);
-                  
-                }, child: Text('')),
-              ),
-              
-               Expanded(
-                 child: TextButton(
-                  
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.purple,
-                    shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
-                    side: BorderSide(color: Colors.purple)
-                  ),
-                  
-                  onPressed: (){
-                   playsound(7);
-                   
-                 }, child: Text('')),
-               ),
+                 buildkey(soundnumber: 1, color: Colors.red),
+                 buildkey(soundnumber: 2, color: Colors.orange),
+                 buildkey(soundnumber: 3, color: Colors.yellow),
+                 buildkey(soundnumber: 4, color: Colors.green),
+                 buildkey(soundnumber: 5, color: Colors.teal),
+                 buildkey(soundnumber: 6, color: Colors.blue),
+                 buildkey(soundnumber: 7, color: Colors.purple),
+                
             ],
           )
         ),
